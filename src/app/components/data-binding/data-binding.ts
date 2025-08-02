ce68a1ad66ddc69f1c52cc59dbb2a7a62b7697f4
stevyMNS <salvadorstevy@gmail.com>
2025-08-02 22:45:07 +0100

    Implement data binding component with dynamic inputs and event handling

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css'
 })
 export class DataBinding {
+ courseName: string = "Angular 20 tutorial";
+ productPrice: number =  100;
+ maxLength: number = 5;
+ minChar: number = 3;
+ inputType: string = "checkbox";
+ myClassName: string = "myColor";
 
+ constructor() {
+  
+ }
+
+  showWelcomeMessage() {
+    alert("Welcome to Angular 20 tutorial");
+  }
+  
+  changeCourseName() {
+    this.courseName = "Reactjs Tutorial"
+  }
+
+  onCityChange() {
+    alert("City changed");
+  }
 }
